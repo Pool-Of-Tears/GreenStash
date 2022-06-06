b64091a15035757530e5f59b28d616ac94db82ae
starry69 <starry369126@outlook.com>
2022-06-06 08:57:26 +0530

    Initialize greenstash
    Signed-off-by: starry69 <starry369126@outlook.com>

---
+++ app/src/main/java/com/starry/greenstash/HomeFragment.kt
@@ -0,0 +1,39 @@
+package com.starry.greenstash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.starry.greenstash.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
