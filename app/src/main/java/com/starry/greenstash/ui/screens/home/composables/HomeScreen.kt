a4074fa153e80188765522f843273466419d57ba
starry-shivam <krshivam24@proton.me>
2023-03-15 11:34:30 +0530

    Add copyright headers
    Signed-off-by: starry-shivam <krshivam24@proton.me>

--- app/src/main/java/com/starry/greenstash/ui/screens/home/composables/HomeScreen.kt
+++ app/src/main/java/com/starry/greenstash/ui/screens/home/composables/HomeScreen.kt
@@ -1,3 +1,28 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) [2022 - Present] Stɑrry Shivɑm
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
 package com.starry.greenstash.ui.screens.home.composables
 
 import android.content.Context
@@ -190,7 +215,7 @@ fun HomeScreen(navController: NavController) {
                             Text(
                                 text = stringResource(id = R.string.no_goal_set),
                                 fontWeight = FontWeight.Medium,
-                                fontSize = 16.sp,
+                                fontSize = 18.sp,
                                 modifier = Modifier.padding(start = 12.dp, end = 12.dp)
                             )
 
