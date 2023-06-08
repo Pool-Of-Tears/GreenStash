c979c4a210d0a209229a687a4272f6622c9013af
starry-shivam <krshivam24@proton.me>
2023-06-08 12:23:22 +0530

    Add single choice chip group
    Signed-off-by: starry-shivam <krshivam24@proton.me>

---
+++ app/src/main/java/com/starry/greenstash/ui/common/SelectableChipGroup.kt
@@ -0,0 +1,106 @@
+package com.starry.greenstash.ui.common
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SelectableChipGroup(
+    modifier: Modifier = Modifier
+        .fillMaxWidth()
+        .padding(14.dp),
+    choices: List<String>,
+    defaultChoice: String = ""
+) {
+    var selected by remember { mutableStateOf(defaultChoice) }
+    Row(
+        modifier = modifier
+    ) {
+        choices.forEach { it ->
+            Chip(
+                title = it,
+                selected = selected,
+                onSelected = {
+                    selected = it
+                }
+            )
+        }
+    }
+
+}
+
+@Composable
+fun Chip(
+    title: String,
+    selected: String,
+    onSelected: (String) -> Unit
+) {
+    val isSelected = selected == title
+    Box(
+        modifier = Modifier
+            .padding(end = 10.dp)
+            .height(35.dp)
+            .clip(RoundedCornerShape(24.dp))
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .clickable(
+                onClick = {
+                    onSelected(title)
+                }
+            )
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 10.dp, end = 10.dp, top = 5.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+
+            AnimatedVisibility(visible = isSelected) {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = "check",
+                    tint = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            }
+            Text(
+                text = title,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                fontSize = 16.sp
+            )
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SelectableChipGroupPV() {
+    SelectableChipGroup(choices = listOf("cat", "dog", "snake"), defaultChoice = "dog")
+}
+
+
+
+
+
