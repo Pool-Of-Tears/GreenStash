b0314e6677224f472fc35aa57c424810d97ca0c2
starry-shivam <starry@krsh.dev>
2024-05-22 14:56:13 +0530

    Add missing copyright header in number utils
    Signed-off-by: starry-shivam <starry@krsh.dev>

--- app/src/main/java/com/starry/greenstash/utils/Utils.kt
+++ app/src/main/java/com/starry/greenstash/utils/Utils.kt
@@ -85,21 +85,19 @@ object Utils {
         NumberUtils.formatCurrency(amount, currencyCode)
 
     /**
-     * Get the authenticators based on the Android version.
+     * Retrieves the appropriate authenticators based on the Android version.
      *
-     * For Android 9 and 10, the authenticators are BIOMETRIC_WEAK and DEVICE_CREDENTIAL.
+     * - For Android 9 (Pie) and Android 10 (Q), the authenticators are `BIOMETRIC_WEAK` and `DEVICE_CREDENTIAL`.
+     * - For Android 11 (R) and above, the authenticators are `BIOMETRIC_STRONG` and `DEVICE_CREDENTIAL`.
+     * - For Android versions below 9, while the authenticators `BIOMETRIC_STRONG` and `DEVICE_CREDENTIAL` are not officially supported,
+     *   using them does not result in an error unlike in Android 9 and 10.
      *
-     * For Android 11 and above, the authenticators are BIOMETRIC_STRONG and DEVICE_CREDENTIAL.
+     * More details can be found in the
+     * [official documentation](https://developer.android.com/reference/androidx/biometric/BiometricPrompt.PromptInfo.Builder#setAllowedAuthenticators(int)).
      *
-     * For Android versions below 9, the authenticators are BIOMETRIC_STRONG and DEVICE_CREDENTIAL although they are not supported,
-     * they don't result in any error unlike in Android 9 and 10.
-     *
-     * See https://developer.android.com/reference/androidx/biometric/BiometricPrompt.PromptInfo.Builder#setAllowedAuthenticators(int)
-     * for more information.
-     *
-     * @return The authenticators based on the Android version.
+     * @return The authenticators suitable for the current Android version.
      */
-    fun getAuthenticators() = if (Build.VERSION.SDK_INT == 28 || Build.VERSION.SDK_INT == 29) {
+    fun getAuthenticators() = if (Build.VERSION.SDK_INT in 28..29) {
         BIOMETRIC_WEAK or DEVICE_CREDENTIAL
     } else {
         BIOMETRIC_STRONG or DEVICE_CREDENTIAL
