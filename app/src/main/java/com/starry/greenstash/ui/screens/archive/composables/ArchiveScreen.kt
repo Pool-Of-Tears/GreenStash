be5e750a636368d78db0addb3beb6954c085419f
Stɑrry Shivɑm <touka.krs@gmail.com>
2025-09-11 21:25:25 +0530

    feat: Update Target SDK to Android 16 (36) (#180)
    While at that also update AGP and dependencies
    
    Signed-off-by: starry-shivam <starry@krsh.dev>

--- app/src/main/java/com/starry/greenstash/ui/screens/archive/composables/ArchiveScreen.kt
+++ app/src/main/java/com/starry/greenstash/ui/screens/archive/composables/ArchiveScreen.kt
@@ -63,7 +63,7 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.starry.greenstash.R
 import com.starry.greenstash.database.core.GoalWithTransactions
